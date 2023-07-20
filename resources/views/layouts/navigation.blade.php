2b2a7d06288fb9b7d128a2fcf5c90c2205ba2cc6
waly0214 <waly0214@gmail.com>
2023-07-20 14:35:39 -0700

    s

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,7 +33,7 @@
                         {{ __('Scholarships') }}
                     </x-nav-link>
                 </div>
-                @if ($user->hasRole('Member'))
+                @if ($this->user->hasRole('Member'))
 
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <x-nav-link :href="route('filament.pages.dashboard')" :active="request()->routeIs('filament.pages.dashboard')">
