3e9a7560ef64c107f0e40bb7abc5332da541ab8c
waly0214 <waly0214@gmail.com>
2023-07-24 14:00:28 -0700

    updated desktop widget

--- app/Filament/Resources/MemberResource/Widgets/MemberStatsOverview.php
+++ app/Filament/Resources/MemberResource/Widgets/MemberStatsOverview.php
@@ -17,10 +17,11 @@ class MemberStatsOverview extends BaseWidget
         $users = User::all()->count();
         return [
             Card::make('Total Membership', Member::all()->count()),
+            Card::make('Total System Users', $users),
             Card::make('Total Active Auxiliaries', Auxiliary::all()->count()-1),
             Card::make('Total Auxiliary Members', $auxMembers),
             Card::make('Total Members at Large', $mal),
-            Card::make('Total System Users', $users),
+
         ];
     }
 }
